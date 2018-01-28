f311e2db7d7869bbff99d43dda2dac6c34593c01
blackerrow <erict.blackham@gmail.com>
2018-01-27 22:32:20 -0700

    added shopping cart

---
+++ web/shopping-cart/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+      <html>
+    <head>
+        <title>Week 3 Form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+       <nav>
+        <ul>
+        <li><a href ="checkout.php">checkout</a></li>
+        <li><a href ="cart.php">view cart</a></li>
+        <li><a href ="confirm.php>confirmation</a></li>
+        </ul>
+       </nav>
+    <h1>THINGS TO BUY</h1>
+    <ul>
+        <li>books</li>
+        <li>computer</li>
+        <li>phone</li>
+        <li>pencils</li>
+    </body>
+</html>
+
