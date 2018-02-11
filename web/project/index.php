9959630672384f40180eafad5f02173e0c06e4bc
blackerrow <erict.blackham@gmail.com>
2018-02-11 00:53:31 -0700

    began styling

--- web/project/index.php
+++ web/project/index.php
@@ -15,13 +15,37 @@ $db = get_db();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Scripture List</title>
+    <title>Journal Me</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-<div>
 
-    <h1>Here is a list of Users: </h1>
+<div class="jumbotron">
+    <h1>Welcome to Journal||Me</h1>
+    <p>Journey before destination</p>
+</div>
+<form class="inline-form">
+    <div class="input-group">
+        <input type="email" class="form-control" size="50" placeholder="email address" required>
+        <div class="input-group-btn">
+            <button class="btn btn-success">Get Started</button>
+        </div>
+    </div>
+</form>
+<div class="container-fluid">
+    <h2>About JournalMe</h2>
+    <p>We are family... Lorem ipsum....................</p>
+    <button class="btn btn-default btn-lg">Contact us</button>
+</div>
+<div class="container-fluid grayback">
+    <h2>Here is a list of Users: </h2>
 
     <?php
 
@@ -38,6 +62,11 @@ $db = get_db();
         echo '</p>';
     }
     ?>
+</div>
+<div>
+
+
+
 
 
 </div>
